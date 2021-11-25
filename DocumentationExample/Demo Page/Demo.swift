c87a77c133c129426aa25b485b1e10290fbd1087
Shreesha <shreeshahrao1999@gmail.com>
2021-11-25 17:52:09 +0530

    Second Commit

---
+++ DocumentationExample/Demo Page/Demo.swift
@@ -0,0 +1,129 @@
+// This is a Demo Example
+// Author: Shreesha Rao
+
+import Foundation
+/**
+ Add Two Numbers Using addtwoNumbers Function
+ An Demo Example
+ 
+  Bulleted List
+ - Item 1 , List A
+ - Item 2 , List A
+ - Item 1 , List B
+ 
+ 1. Cat
+ 2. Dog
+         1. Golden Retriever
+ 3. Llama
+ 
+ An Example of heading using a character underline for a heading
+
+   This is a Heading 1
+   =============
+ 
+ Numbered List
+ 
+ 
+ 
+ 
+ Buleted List
+ 
+ * item 1
+ * item 2
+        * item 2.1
+ * item3
+ 
+ 
+  
+  
+ 
+ 
+ 
+ - parameters:
+    - number1: 10
+    - number2 : 20
+    - output : 30
+ 
+ This text is above the horizontal Rule
+   
+  - - -
+   
+  And this is below
+ 
+ This is text is above horizontal rule
+  
+  * * * * * *
+   
+ And this is below
+ 
+ 
+ - Parameter sepreator: text to be printed
+ - Parameter terminator: text to be printed
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
+
+
+
+func addTwoNum(number1: Int , number2: Int) {
+        print(number1 + number2)
+    }
+
+/**
+ 
+ # Multiply Two Numbers
+  
+ A function to multiply two numbers  to display box
+ 
+ Show shift elements for   `for` and `let` as monospaced code font
+  Italics *wdwd*
+   Bold **wddawd**
+   __BOLD__
+ 
+  For more information, see [The Swift programming Language.](http://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/) Linking the a webpage
+ 
+  ````
+  func multiplyNumber(num1: Int, num2: Int) {
+    print(num1 x num2)}
+ ```
+ 
+ */
+
+func multiplyTwoNumbers(num1: Int, num2: Int) {
+    print( num1 * num2)
+}
+
+/**
+  
+  An Example for linking Reference
+  
+ For Linking Reference
+ 
+ [The Swift Programming Language]: http://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/ "Some hover text"
+
+  For Reading We Sugest [The Swift Programming Language].
+   
+ [Next page](@Demo2)
+
+ ## Heading 2 String
+ 
+ * [The Swift Programming Language].
+ 
+ */
+
+
+
+
+func subtractTwoNumbers(num1: Int, num2: Int) {
+    print(num1 - num2)
+}
+
+
+
+
+
---
+++ DocumentationExample/Demo Page/Demo2.swift
@@ -0,0 +1,206 @@
+//
+//  Demo2.swift
+//  DocumentationExample
+//
+//  Created by Shreesha on 25/11/21.
+//
+
+
+
+
+import Foundation
+import UIKit
+
+/**
+  An example of using the bug field
+ 
+  - Bug: [*bugExample* contains a memory leak](BugDB://problem/1367823)
+ 
+  - Bug: [Passing a `UIViewController` crashes *bugExample*](BugDB://problem/2274610)
+ */
+   
+func showCallouts2() {
+    
+}
+/**
+   An Example of using  Callouts
+     
+ 
+    
+    - Attention: What if i told you wrong?
+    - Author: Shreesha Rao
+    - Authors: Shreesha Rao,  Adarsh
+     
+ */
+
+func showCallouts() {
+    print("All Callouts")
+}
+
+/**
+    Example of using Complexity Field
+        
+   - Complexity: The method demonstrates an inefficient way to sort using an 0(N\*N\*N) (ordered N-cubed) algorithm
+ */
+func addingComplexity() {
+    
+}
+
+/**
+  Example of using copyright field
+   
+   - Copyright: Copyright @ 2015 by the group of Barraons
+ 
+ */
+func addCopyrightField() {
+    
+}
+/**
+ Example of a Callout
+ 
+ * Callout(Llama Spotting Tips): Packs warm clothes with your binoculars.
+ */
+func calloutTips() {
+    
+}
+
+/**
+ An Example of using date field
+ 
+  Last date this example was changed
+ 
+ */
+func searchDate() {
+    
+}
+/**
+  - Example: *A Simple `for` loop.*\
+        This Example shows a for loop that prints the number 1 to 5
+                
+                for index in 1...5 {
+                    print("index = \(index)")
+                }
+ */
+func toPrintExample(){
+    
+}
+/**
+ Imortant 
+ 
+ 
+ */
+func Important() {
+    
+}
+/**
+  Experimnet
+ 
+  
+ 
+ 
+
+ */
+func Experiment() {
+    
+}
+/**
+    How to show Diffrent Authors
+ 
+    
+            
+    Milynw
+ 
+    wdadwdw
+ */
+
+func showCallouts1() {
+    print("All Callouts")
+}
+/**
+    An Example of Invariant
+        
+ 
+ */
+func showInvariant() {
+    
+}
+/**
+    Example of Note
+ 
+    - Note: "There is either good or bad, but thinking makes it so."
+ 
+    Hamlet is good
+       
+    - Precondition: The `Person` property must be non-nil.
+    - Precondition: `Update` Address
+    - PostCondition: After completing this method the billing address for
+ 
+    the person will be set to `updateAddress` if it is valid.
+ 
+    Otherwise the billing address will not be changed
+    
+    
+    - Remark: The performance could be reduced from N-Squared to N-log-N by switching patterns.
+ 
+    - Invariant: The Person reference will not change during the execution of this method
+ 
+    - Authors: Shreesha , Roman
+ 
+ 
+    * Experiment:
+ 
+    Find out if u are tasing your food
+ 
+        1. Get a few Diffrent foods
+        2. Cut Them Up
+    
+    - Important: " The begginning is the most important part of the work ." \-Plato
+  
+    - Parameters:
+        - number1: 10
+        - number2: 20
+        - divideNumber:
+ 
+    - Date: August 19 , 2021
+ 
+    Days the method produces \*special results
+ 
+    - Date: 12/31
+    - Date:  03/17
+ 
+    Image Name
+  
+    ![Apple](https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fnature%2F&psig=AOvVaw3xjVqye4modC4nCK5pfd5v&ust=1637908985569000&source=images&cd=vfe&ved=2ahUKEwjY_KvL9LL0AhUER2wGHeVnBfoQjRx6BAgAEAk)
+ 
+    - Requires: `start <= end`.
+        
+    - SeeAlso:
+ 
+    See Reference [My Library](https://www.google.com)
+        
+    - Since: First availabe in Mac OS 7
+     
+    - Version: 1.0
+ 
+    - Warning: Not all code paths for this method have been tested
+   
+    - Complexity: This Method Demonstrates Complexity
+ 
+    * Callout(Llama Spoting Tips)
+ 
+        Pack warm clothes with your tools
+ 
+    - Example: *A simple `for` loop.*\
+    This example shows a `for` loop that prints the numbers 1 to 5.\
+    \
+    `for index in 1...5 {`\
+    `   print("index = \(index)")`\
+    `}`}
+ 
+ */
+func makeNoteandPrecondition() {
+    /**
+        - Note: This is a number Declartion
+     */
+    var number: Int
+}
